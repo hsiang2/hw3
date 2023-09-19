fa9987853452e22d24d4dea3ef988c776a2007fd
hsiang2 <q224399377@gmail.com>
2023-09-19 18:00:05 +0800

    Set up

---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom"
+import { Button, Input } from "antd"
+import type { SearchProps } from "antd/es/input";
+
+const { Search } = Input
+const onSearch: SearchProps['onSearch'] = (value, _e, info) => console.log(info?.source, value);
+
+const Header = () => {
+    return (
+        // <div className="header">
+        <div className="container header">
+            <div className="nav-left">
+                <Link to="/">
+                    BOOKS
+                </Link>
+                <Search placeholder="Find Books..." onSearch={onSearch}  />
+            </div>
+            <Button type="primary" shape="round">ADD NEW BOOKS</Button>
+        </div>
+        // </div>
+    )
+}
+
+export default Header
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,17 @@
+import { Row } from "antd"
+import Header from "../components/Header"
+
+const HomePage = () => {
+    return (
+        <>
+            <Header />
+            <div className="container">
+                <Row >
+
+                </Row>
+            </div>
+        </>
+    )
+}
+
+export default HomePage
